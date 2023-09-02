c9be5c6263985b7df60bcc605bdd2f016d1c5d82
Geleta116 <geleta.dabakumsa@gmail.com>
2023-09-02 10:35:19 +0300

    setup the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,18 @@ import { RatingModule } from './rating/rating.module';
 import { ApplicationModule } from './application/application.module';
 import { AuthModule } from './auth/auth.module';
 import { ProjectModule } from './project/project.module';
-
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     ReviewModule,
     RatingModule,
     ApplicationModule,
     AuthModule,
     ProjectModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
