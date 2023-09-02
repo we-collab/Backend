95d61a55a121f6f72ec0a04b11915d7f32d3848e
Geleta116 <geleta.dabakumsa@gmail.com>
2023-09-02 11:41:48 +0300

    add validation to update user dto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,12 +1,44 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
   id: number;
+
+  @IsString()
+  @IsOptional()
   name: string;
+
+  @IsEmail()
+  @IsOptional()
   email: string;
+
+  @IsString()
+  @IsOptional()
   picture: string;
-  password: string;
+
+  @IsString()
+  @IsOptional()
   pitch?: string | null;
+
+  @IsString()
+  @IsOptional()
   about?: string | null;
+
+  @IsString()
+  @IsOptional()
   experience?: string | null;
+
+  @IsBoolean()
+  @IsOptional()
   availability: boolean;
+
+  @IsNumber()
+  @IsOptional()
   money: number;
 }
